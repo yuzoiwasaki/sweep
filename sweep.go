cc4d3c4bbc38fb39944bf4691958564a20c739a4
yuzoiwasaki <yuzoiwasaki0929@gmail.com>
2020-04-13 17:06:44 +0900

    Update command

--- sweep.go
+++ sweep.go
@@ -11,7 +11,7 @@ func main() {
 		b = flag.String("exclude", "master", "string flag")
 	)
 	flag.Parse()
-	cmdstr := "git branch | egrep -v " + *b + " | xargs git branch -D"
+	cmdstr := "git branch | grep -v " + *b + " | xargs git branch -D"
 	err := exec.Command("sh", "-c", cmdstr).Run()
 	if err != nil {
 		fmt.Printf("Error! Failed to sweep branches.\n")
