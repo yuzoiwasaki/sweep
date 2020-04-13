b3d0e237fc1131161ea034ef5becfda531aecc60
yuzoiwasaki <yuzoiwasaki0929@gmail.com>
2020-04-13 12:59:17 +0900

    Create sweep command

---
+++ sweep.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os/exec"
+)
+
+func main() {
+	var (
+		b = flag.String("exclude", "master", "string flag")
+	)
+	flag.Parse()
+	cmdstr := "git branch | egrep -v " + *b + " | xargs git branch -D"
+	err := exec.Command("sh", "-c", cmdstr).Run()
+	if err != nil {
+		fmt.Printf("Error! Failed to sweep branches.\n")
+	}
+}
