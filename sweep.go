0baec84a69b43b9b3643eb9541092f1feaef4cc2
yuzoiwasaki <yuzoiwasaki0929@gmail.com>
2020-07-11 15:54:26 +0900

    Supports multiple arguments

--- sweep.go
+++ sweep.go
@@ -6,12 +6,30 @@ import (
 	"os/exec"
 )
 
+type strslice []string
+
+func (s *strslice) String() string {
+	return fmt.Sprintf("%v", multiflag)
+}
+
+func (s *strslice) Set(v string) error {
+	*s = append(*s, v)
+	return nil
+}
+
+var multiflag strslice = strslice{"master"}
+
 func main() {
-	var (
-		b = flag.String("exclude", "master", "string flag")
-	)
+	flag.Var(&multiflag, "exclude", "string flag")
 	flag.Parse()
-	cmdstr := "git branch | grep -v " + *b + " | xargs git branch -D"
+
+	var excludeCmd string
+
+	for _, s := range multiflag {
+		excludeCmd = excludeCmd + " | grep -v " + s
+	}
+	cmdstr := "git branch" + excludeCmd + " | xargs git branch -D"
+
 	err := exec.Command("sh", "-c", cmdstr).Run()
 	if err != nil {
 		fmt.Printf("Error! Failed to sweep branches.\n")
