6d6b2e20c087f34a41a89c45396b2f805de46dfc
yuzoiwasaki <yuzoiwasaki0929@gmail.com>
2020-07-11 23:42:00 +0900

    Refactor

--- sweep.go
+++ sweep.go
@@ -17,18 +17,18 @@ func (s *strslice) Set(v string) error {
 	return nil
 }
 
-var multiflag strslice = strslice{"master"}
+var multiflag = strslice{"master"}
 
 func main() {
 	flag.Var(&multiflag, "exclude", "Set exclude branch")
 	flag.Parse()
 
-	var excludecmd string
+	var excludeCmd string
 
 	for _, s := range multiflag {
-		excludecmd = excludecmd + " | grep -v " + s
+		excludeCmd = excludeCmd + " | grep -v " + s
 	}
-	cmdstr := "git branch" + excludecmd + " | xargs git branch -D"
+	cmdstr := "git branch" + excludeCmd + " | xargs git branch -D"
 
 	err := exec.Command("sh", "-c", cmdstr).Run()
 	if err != nil {
